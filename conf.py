1e413bff816b09b92a739f2691e33e036216f0a1
mnug <github@maxwellnugent.com>
2024-04-23 01:28:37 +0100

    dhghfjh

--- conf.py
+++ conf.py
@@ -19,9 +19,6 @@
 root_doc = "docs/index"
 
 
-
-
-
 # -- Project information -----------------------------------------------------
 
 project = "Basic Sphinx Example Project"
@@ -68,3 +65,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+
+print("AAAAAAAAAAAAAAAAAAAAAAAAA")
